c0886fdaa1e7ba25a5298bee11276e25c6446c03
liviabeatrizml <liviabeatrizmaia7@gmail.com>
2023-04-23 12:42:33 -0300

    atualização comandos SQL

--- GradeHorariaComandos.sql
+++ GradeHorariaComandos.sql
@@ -1,10 +1,9 @@
 create table professor
-(matricula varchar(20),
-nome varchar(60),
+(nome varchar(60),
 titulacao varchar(60),
 email varchar(60),
 departamento varchar(20),
-primary key (matricula));
+primary key (email));
 
 create table componenteCurricular
 (codigo varchar(20),
@@ -14,5 +13,12 @@ descricao varchar(20),
 periodo int,
 primary key (codigo));
 
-insert into componenteCurricular (codigo, nome, cargaHoraria, descricao, periodo)
-values ('PEX1248', 'Banco de Dados', 60, 'Obrigatória', 4);
+create table turma
+(nome varchar(20),
+ano int,
+semestre int,
+diasDaSemana varchar(20),
+horarios varchar(20),
+turno varchar(20),
+bloco varchar(20),
+vagas int);
